ba88d63f70ded0bf2cc8a6f9a0054c611f19a6b0
sebas1201 <jnaffahi@pslcol.com.co>
2016-05-27 15:41:50 -0500

    Employeee

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+// getElementById
+    function $id(id) {
+        return document.getElementById(id);
+    }
 
+    //
+    // output information
+    function Output(msg) {
+        var m = $id("messages");
+        m.innerHTML = msg + m.innerHTML;
+    }
